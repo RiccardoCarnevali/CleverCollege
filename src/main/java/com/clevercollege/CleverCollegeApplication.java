9a4bbc7f22c159df332e27e6e4be6dbef22ef313
Riccardo Carnevali <riccardo.carnevali@gmail.com>
2021-12-19 18:15:57 +0100

    Aggiunto scheletro del progetto

---
+++ src/main/java/com/clevercollege/CleverCollegeApplication.java
@@ -0,0 +1,13 @@
+package com.clevercollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverCollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverCollegeApplication.class, args);
+	}
+
+}
